73c2a788d13d2930b1463bd29cd91c5a4556a40e
Jochen Bernard <jochenbernard@gmail.com>
2026-07-18 13:59:05 +0200

    Add SwiftLint

--- Package.swift
+++ Package.swift
@@ -4,10 +4,24 @@ import PackageDescription
 
 let package = Package(
     name: "SousKit",
+    platforms: [
+        .iOS(.v15),
+        .macCatalyst(.v15),
+        .macOS(.v12),
+        .tvOS(.v15),
+        .visionOS(.v1),
+        .watchOS(.v8)
+    ],
     products: [
         .library(
             name: "SousKit",
             targets: ["SousKit"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/SimplyDanny/SwiftLintPlugins",
+            from: "0.1.0"
         )
     ],
     targets: [
@@ -17,6 +31,12 @@ let package = Package(
                 .enableUpcomingFeature("InferIsolatedConformances"),
                 .enableUpcomingFeature("MemberImportVisibility"),
                 .enableUpcomingFeature("NonisolatedNonsendingByDefault")
+            ],
+            plugins: [
+                .plugin(
+                    name: "SwiftLintBuildToolPlugin",
+                    package: "SwiftLintPlugins"
+                )
             ]
         )
     ],
